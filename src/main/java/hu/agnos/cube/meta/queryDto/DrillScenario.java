c1bd9e2a9dabbb679da3aa13373ecf34b6f86fd9
ruzsaz <ruzsaz@gmail.com>
2023-11-08 15:48:35 +0100

    KM calculation woring in the old way

--- src/main/java/hu/agnos/cube/meta/queryDto/DrillScenario.java
+++ src/main/java/hu/agnos/cube/meta/queryDto/DrillScenario.java
@@ -10,16 +10,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum DrillScenario {
 
-    DRILL(true, true, false),
-    DRILL_FOR_EXTRA_CALCULATIONS(true, false, false),
-    NOT_REQUESTED(false, false, false),
-    REQUESTED_BUT_ALLOWED_LEVEL_REACHED(false, true, false),
-    REQUESTED_BUT_LEVEL_MISSING(false, false, true),
-    FOR_EXTRA_CALCULATIONS_BUT_ALLOWED_LEVEL_REACHED(false, false, false),
-    FOR_EXTRA_CALCULATIONS_BUT_LEVEL_MISSING(false, false, false);
+    DRILL(true, true, false, false),
+    DRILL_AND_EXTRA_CALCULATIONS(true, true, false, true),
+    ONLY_FOR_EXTRA_CALCULATIONS(true, false, false, true),
+    NOT_REQUESTED(false, false, false, false),
+    REQUESTED_BUT_ALLOWED_LEVEL_REACHED(false, true, false, false),
+    REQUESTED_BUT_LEVEL_MISSING(false, false, true, false),
+    FOR_EXTRA_CALCULATIONS_BUT_ALLOWED_LEVEL_REACHED(false, false, false, false),
+    FOR_EXTRA_CALCULATIONS_BUT_LEVEL_MISSING(false, false, false, false);
 
     private final boolean required; // Shows if drill is required in the dimension
     private final boolean showResultAsDimValue; // Indicates that this dimension's coordinate values should be shown in the requested drill
     private final boolean showResultAsParentsValue; // Shows if the value of the drill should be read from the parent's value
+    private final boolean showExtraCalculationInstead; // Instead of showing the obtained value, apply extra calculations
 
 }
